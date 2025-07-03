4716953ba7682785d553e4689951d1cc6781eeff
White Macro <whitemacroyt@gmail.com>
2025-07-02 22:16:36 -0600

    Testing github

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger, SplitText);
+const App = () => {
+    return (
+        <div className="flex-center h-[100vh]">
+            <h1 className="text-7xl text-indigo-350">Testing 1,2,3</h1>
+        </div>
+    )
+}
+export default App
